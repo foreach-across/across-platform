fe5b218e217a6b478d1f120da45fc39d220b92c3
Marc Vanbrabant <marc@foreach.be>
2016-11-24 11:50:15 +0100

    Allow arguments to be passed (makes "overrides" in Intellij Spring Boot config work)

--- platform-bom-test/src/main/java/com/foreach/across/modules/platform/PlatformTestApplication.java
+++ platform-bom-test/src/main/java/com/foreach/across/modules/platform/PlatformTestApplication.java
@@ -42,7 +42,7 @@ public class PlatformTestApplication extends SpringBootServletInitializer
 {
 
 	public static void main( String[] args ) {
-		SpringApplication.run( PlatformTestApplication.class );
+		SpringApplication.run( PlatformTestApplication.class, args );
 	}
 
 	@Bean
