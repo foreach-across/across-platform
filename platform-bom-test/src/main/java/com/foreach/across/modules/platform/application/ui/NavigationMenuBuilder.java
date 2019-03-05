a415ee4dcfad7a963cb95cca363132cc9b5a7c42
Arne Vandamme <arne@foreach.be>
2019-03-05 20:39:26 +0100

    Platform 4.0.0

--- platform-bom-test/src/main/java/com/foreach/across/modules/platform/application/ui/NavigationMenuBuilder.java
+++ platform-bom-test/src/main/java/com/foreach/across/modules/platform/application/ui/NavigationMenuBuilder.java
@@ -1,13 +1,12 @@
 package com.foreach.across.modules.platform.application.ui;
 
-import com.foreach.across.core.annotations.Event;
-import com.foreach.across.core.annotations.EventName;
 import com.foreach.across.modules.platform.application.business.Category;
 import com.foreach.across.modules.web.events.BuildMenuEvent;
 import com.foreach.across.modules.web.menu.Menu;
 import com.foreach.across.modules.web.menu.PathBasedMenuBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.event.EventListener;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -24,8 +23,8 @@ public class NavigationMenuBuilder
 		this.messageSource = messageSource;
 	}
 
-	@Event
-	public void buildMainMenu( @EventName("navigationMenu") BuildMenuEvent<Menu> menuEvent ) {
+	@EventListener(condition = "#menuEvent.eventName == 'navigationMenu'")
+	public void buildMainMenu( BuildMenuEvent<Menu> menuEvent ) {
 		menuEvent.builder()
 		         .item( "/home", message( "nav.home" ), "/" ).order( 0 ).and()
 		         .group( "/category", message( "nav.browse" ) ).order( 1 ).and()
